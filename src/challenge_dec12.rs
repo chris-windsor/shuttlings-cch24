1b384d8324590f0624ef1f458a9549ef98810236
Chris Windsor <ccw3102@icloud.com>
2024-12-17 07:39:50 -0700

    complete core tasks for dec 12th challenge

--- src/challenge_dec12.rs
+++ src/challenge_dec12.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, usize};
+use std::{fmt::Display, sync::Arc, usize};
 
 use axum::{
     extract::{Path, State},
@@ -10,8 +10,22 @@ use crate::AppState;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum BoardTile {
+    Wall,
+    Blank,
     Cookie,
     Milk,
+}
+
+impl Display for BoardTile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let emoji = match self {
+            BoardTile::Wall => "⬜",
+            BoardTile::Blank => "⬛",
+            BoardTile::Cookie => "🍪",
+            BoardTile::Milk => "🥛",
+        };
+        write!(f, "{}", emoji)
+    }
 }
 
 #[derive(Debug)]
@@ -19,11 +33,6 @@ pub struct MilkCookieGame {
     board_state: Vec<Vec<BoardTile>>,
     size: usize,
 }
-
-const WALL_TILE: &str = "⬜";
-const BLANK_TILE: &str = "⬛";
-const COOKIE_TILE: &str = "🍪";
-const MILK_TILE: &str = "🥛";
 
 impl MilkCookieGame {
     pub fn new(size: usize) -> Self {
@@ -42,30 +51,126 @@ impl MilkCookieGame {
         column.push(tile);
     }
 
+    fn check_columns(&self, tile: BoardTile) -> bool {
+        for column in self.board_state.clone() {
+            if column.len() == self.size && column.iter().all(|cell| cell == &tile) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn check_rows(&self, tile: BoardTile) -> bool {
+        for row in 0..self.size {
+            let row = self
+                .board_state
+                .iter()
+                .filter_map(|column| {
+                    if let Some(cell) = column.get(row) {
+                        return Some(*cell);
+                    }
+                    None
+                })
+                .collect::<Vec<_>>();
+            if row.len() == self.size && row.iter().all(|cell| cell == &tile) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn check_diagonals(&self, tile: BoardTile) -> bool {
+        let mut row = 0;
+        let mut diag: Vec<BoardTile> = Vec::new();
+        for column in 0..self.size {
+            let cell = self.board_state.get(column).unwrap().get(row);
+            if let Some(cell) = cell {
+                diag.push(*cell);
+            }
+            row += 1;
+        }
+
+        if diag.len() == self.size && diag.iter().all(|cell| cell == &tile) {
+            return true;
+        }
+
+        let mut diag: Vec<BoardTile> = Vec::new();
+        let mut row = self.size;
+
+        for column in 0..self.size {
+            let cell = self.board_state.get(column).unwrap().get(row - 1);
+            if let Some(cell) = cell {
+                diag.push(*cell);
+            }
+            row -= 1;
+        }
+
+        if diag.len() == self.size && diag.iter().all(|cell| cell == &tile) {
+            return true;
+        }
+
+        false
+    }
+
+    fn is_game_over(&self) -> bool {
+        self.board_state
+            .iter()
+            .all(|column| column.len() == self.size)
+    }
+
+    fn get_winner(&self) -> Option<String> {
+        for tile in [BoardTile::Cookie, BoardTile::Milk] {
+            let won_column = self.check_columns(tile);
+            let won_row = self.check_rows(tile);
+            let won_diag = self.check_diagonals(tile);
+
+            if won_column || won_row || won_diag {
+                return Some(format!("{tile} wins!"));
+            }
+        }
+
+        None
+    }
+
+    fn can_play(&self) -> bool {
+        !self.get_winner().is_some()
+    }
+
     fn to_string(&self) -> String {
         let mut board = String::new();
         for row in 0..self.size {
-            let mut new_line: String = WALL_TILE.to_string();
+            let mut new_line: String = BoardTile::Wall.to_string();
             for column in 0..self.size {
                 let column = self.board_state.get(column).unwrap();
                 let cell = column.get(self.size - row - 1);
                 if let Some(existing_tile) = cell {
-                    match existing_tile {
-                        &BoardTile::Cookie => new_line.push_str(COOKIE_TILE),
-                        &BoardTile::Milk => new_line.push_str(MILK_TILE),
-                    }
+                    new_line.push_str(&existing_tile.to_string());
                 } else {
-                    new_line.push_str(BLANK_TILE);
+                    new_line.push_str(&BoardTile::Blank.to_string());
                 }
             }
 
-            new_line.push_str(WALL_TILE);
+            new_line.push_str(&BoardTile::Wall.to_string());
             new_line.push_str("\n");
             board.push_str(&new_line);
         }
 
-        board.push_str(&WALL_TILE.repeat(self.board_state.len() + 2));
+        board.push_str(
+            &BoardTile::Wall
+                .to_string()
+                .repeat(self.board_state.len() + 2),
+        );
         board.push_str("\n");
+
+        if let Some(winner_message) = self.get_winner() {
+            board.push_str(&winner_message);
+            board.push_str("\n");
+        }
+
+        if self.is_game_over() {
+            board.push_str(&format!("No winner.\n"));
+        }
+
         board
     }
 }
@@ -93,12 +198,23 @@ pub async fn milk_cookie_game_place(
         return Err(Day12AppError::BadPlacement);
     }
 
+    if !state.milk_cookie_game.try_read().unwrap().can_play() {
+        return Ok((
+            StatusCode::SERVICE_UNAVAILABLE,
+            state.milk_cookie_game.try_read().unwrap().to_string(),
+        ));
+    }
+
     state
         .milk_cookie_game
         .try_write()
         .unwrap()
         .place(tile_kind, column);
-    Ok(state.milk_cookie_game.try_read().unwrap().to_string())
+
+    Ok((
+        StatusCode::OK,
+        state.milk_cookie_game.try_read().unwrap().to_string(),
+    ))
 }
 
 pub enum Day12AppError {
