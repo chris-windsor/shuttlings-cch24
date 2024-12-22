39dadd1ee87a1b751b2d8625bcf525a5a1615679
Chris Windsor <ccw3102@icloud.com>
2024-12-21 23:52:34 -0700

    complete core tasks for dec 19th challenge

---
+++ src/challenge_dec19.rs
@@ -0,0 +1,139 @@
+use std::sync::Arc;
+
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::{
+    types::{
+        chrono::{DateTime, Utc},
+        Uuid,
+    },
+    FromRow,
+};
+
+use crate::AppState;
+
+pub async fn quotes_reset(state: State<Arc<AppState>>) -> impl IntoResponse {
+    sqlx::query("TRUNCATE quotes")
+        .execute(&state.pool)
+        .await
+        .unwrap();
+    StatusCode::OK
+}
+
+#[derive(Deserialize)]
+pub struct QuoteInsert {
+    author: String,
+    quote: String,
+}
+
+#[derive(FromRow, Serialize)]
+struct Quote {
+    id: Uuid,
+    author: String,
+    quote: String,
+    created_at: DateTime<Utc>,
+    version: i32,
+}
+
+pub async fn quote_by_id(
+    state: State<Arc<AppState>>,
+    Path(id): Path<Uuid>,
+) -> Result<impl IntoResponse, Day19AppError> {
+    let quote = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
+        .bind(id)
+        .fetch_one(&state.pool)
+        .await?;
+    Ok((StatusCode::OK, Json(quote)))
+}
+
+pub async fn remove_quote_by_id(
+    state: State<Arc<AppState>>,
+    Path(id): Path<Uuid>,
+) -> Result<impl IntoResponse, Day19AppError> {
+    let quote = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
+        .bind(id)
+        .fetch_one(&state.pool)
+        .await?;
+
+    sqlx::query("DELETE FROM quotes WHERE id = $1")
+        .bind(id)
+        .execute(&state.pool)
+        .await?;
+    Ok((StatusCode::OK, Json(quote)))
+}
+
+pub async fn undo_quote_by_id(
+    state: State<Arc<AppState>>,
+    Path(id): Path<Uuid>,
+    Json(new_quote): Json<QuoteInsert>,
+) -> Result<impl IntoResponse, Day19AppError> {
+    sqlx::query(
+        "UPDATE quotes SET (version, quote, author) = (version+1, $1, $2)
+  WHERE id = $3",
+    )
+    .bind(new_quote.quote)
+    .bind(new_quote.author)
+    .bind(&id)
+    .execute(&state.pool)
+    .await?;
+
+    let quote = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
+        .bind(&id)
+        .fetch_one(&state.pool)
+        .await?;
+
+    Ok((StatusCode::OK, Json(quote)))
+}
+
+pub async fn draft_quote(
+    state: State<Arc<AppState>>,
+    Json(new_quote): Json<QuoteInsert>,
+) -> Result<impl IntoResponse, Day19AppError> {
+    let id = Uuid::new_v4();
+    let utc: DateTime<Utc> = Utc::now();
+
+    sqlx::query("INSERT INTO quotes (id, created_at, author, quote) VALUES ($1, $2, $3, $4)")
+        .bind(&id)
+        .bind(utc)
+        .bind(new_quote.author)
+        .bind(new_quote.quote)
+        .execute(&state.pool)
+        .await?;
+
+    let quote = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
+        .bind(&id)
+        .fetch_one(&state.pool)
+        .await?;
+
+    Ok((StatusCode::CREATED, Json(quote)))
+}
+
+pub enum Day19AppError {
+    SqlXError(sqlx::Error),
+}
+
+impl IntoResponse for Day19AppError {
+    fn into_response(self) -> Response {
+        match self {
+            Day19AppError::SqlXError(rejection) => {
+                println!("{}", rejection);
+                match rejection {
+                    sqlx::Error::RowNotFound => (StatusCode::NOT_FOUND, ""),
+                    _ => (StatusCode::BAD_REQUEST, ""),
+                }
+            }
+        }
+        .into_response()
+    }
+}
+
+impl From<sqlx::Error> for Day19AppError {
+    fn from(rejection: sqlx::Error) -> Self {
+        Self::SqlXError(rejection)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use axum::{
-    routing::{get, post},
+    routing::{delete, get, post, put},
     Router,
 };
 use challenge_dec12::{
@@ -12,6 +12,9 @@ use challenge_dec12::{
     MilkCookieGame,
 };
 use challenge_dec16::{unwrap_encrypted_present, unwrap_present, wrap_present};
+use challenge_dec19::{
+    draft_quote, quote_by_id, quotes_reset, remove_quote_by_id, undo_quote_by_id,
+};
 use challenge_dec2::{
     egregious_encryption_dest, egregious_encryption_dest_v6, egregious_encryption_key,
     egregious_encryption_key_v6,
@@ -20,9 +23,11 @@ use challenge_dec5::car_go_festivity;
 use challenge_dec9::{milk_bucket_leaky, milk_bucket_refill};
 use challenge_intro::{hello_bird, seek_and_find};
 use leaky_bucket_lite::LeakyBucket;
+use shuttle_runtime::CustomError;
 
 mod challenge_dec12;
 mod challenge_dec16;
+mod challenge_dec19;
 mod challenge_dec2;
 mod challenge_dec5;
 mod challenge_dec9;
@@ -31,10 +36,16 @@ mod challenge_intro;
 struct AppState {
     leaky_milk_bucket: LeakyBucket,
     milk_cookie_game: RwLock<MilkCookieGame>,
+    pool: sqlx::PgPool,
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_shared_db::Postgres] pool: sqlx::PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(CustomError::new)?;
+
     let leaky_milk_bucket = LeakyBucket::builder()
         .max(5)
         .tokens(5)
@@ -47,6 +58,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let app_state = Arc::new(AppState {
         leaky_milk_bucket,
         milk_cookie_game,
+        pool,
     });
 
     let router = Router::new()
@@ -66,6 +78,11 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/16/wrap", post(wrap_present))
         .route("/16/unwrap", get(unwrap_present))
         .route("/16/decode", post(unwrap_encrypted_present))
+        .route("/19/reset", post(quotes_reset))
+        .route("/19/cite/:id", get(quote_by_id))
+        .route("/19/remove/:id", delete(remove_quote_by_id))
+        .route("/19/undo/:id", put(undo_quote_by_id))
+        .route("/19/draft", post(draft_quote))
         .with_state(app_state);
 
     Ok(router.into())
